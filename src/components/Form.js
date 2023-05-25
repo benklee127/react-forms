f3108687e26ea0bf586bc160985fc436a22d19bb
Edward <eghuffstetler@gmail.com>
2023-05-25 18:18:17 -0400

    working on error

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,69 @@
-
-
+import { useState } from 'react';
 
 export default function Form () {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [phone, setPhone] = useState();
+    const [phoneType, setPhoneType] = useState();
+    const [bio, setBio] = useState();
+    const [role, setRole] = useState();
+    const [emailNotice, setEmailNotice] = useState();
+    const [validationErrors, setValidationErrors] = useState();
+
+    const validate = () => {
+        const errors = {};
+
+        if(!name)
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(name);
+        console.log(email);
+        console.log(phone);
+        console.log(phoneType);
+        console.log(bio);
+        console.log(role);
+        console.log(emailNotice);
+
+        setName();
+        setEmail();
+        setPhone();
+        setPhoneType();
+        setBio();
+        setRole();
+        setEmailNotice();
+
+        const formInfo = {
+            name,
+            email,
+            phone,
+            phoneType,
+            bio,
+            role,
+            emailNotice
+        };
+
+        console.log(formInfo);
+    }
+
     return (
         <form>
             <label >
                 Name
-                <input name='name' type='text'/>
+                <input id='name' type='text' value={name} onChange={e => setName(e.target.value)}/>
             </label>
             <label >
                 Email
-                <input name='email' type='email'/>
+                <input id='email' type='email' onChange={e => setEmail(e.target.value)}/>
             </label>
             <label >
                 Phone Number
-                <input name='phoneNum' type='tel'/>
+                <input id='phoneNum' type='tel' onChange={e => setPhone(e.target.value)}/>
             </label>
             <label >
                 Phone type
-                <select name='phoneType'>
+                <select name='phoneType' onChange={e => setPhoneType(e.target.value)}>
                     <option value="Home">Home</option>
                     <option value="Work">Work</option>
                     <option value="Mobile">Mobile</option>
@@ -26,20 +71,21 @@ export default function Form () {
             </label>
             <label >
                 Instructor
-                <input name='role' type='radio' value='instructor'/>
+                <input id='role' type='radio' value='instructor'/>
             </label>
             <label >
                 Student
-                <input name='role' type='radio' value='student'/>
+                <input id='role' type='radio' value='student'/>
             </label>
             <label >
                 Bio
-                <textarea name='bio' placeholder = 'Please input a bio'></textarea>
+                <textarea id='bio' placeholder = 'Please input a bio' onChange={e => setBio(e.target.value)}></textarea>
             </label>
             <label >
-                Name
-                <input name='Name' type='text'/>
+                Sign up for email notification
+                <input id='emailNotice' type='checkbox' onChange={e => setEmailNotice(e.target.value)}/>
             </label>
+            <button type="submit" onClick={onSubmit}>Submit</button>
         </form>
         )
 }
