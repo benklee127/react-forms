6cabac44ed704e4bbbfb0336395dacce6d005ab2
Edward <eghuffstetler@gmail.com>
2023-05-27 16:23:38 -0400

    finished main part

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,17 @@
 import { useState } from 'react';
 
 export default function Form () {
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [phone, setPhone] = useState();
-    const [phoneType, setPhoneType] = useState();
-    const [bio, setBio] = useState();
-    const [role, setRole] = useState();
-    const [emailNotice, setEmailNotice] = useState();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [phoneType, setPhoneType] = useState('');
+    const [bio, setBio] = useState('');
+    const [role, setRole] = useState('Student');
+    const [emailNotice, setEmailNotice] = useState('off');
     const [validationErrors, setValidationErrors] = useState({});
 
     const validate = () => {
         const errors = {};
-
-        console.log(name);
-        console.log('email', email);
-        console.log(phone);
-        console.log(phoneType);
-        console.log(bio);
-        console.log(role);
-        console.log(emailNotice);
 
         if(!name) errors.name = "Name is required";
         if(!email) errors.email = "Email is required";
@@ -28,7 +20,6 @@ export default function Form () {
 
         if(phone) {
             const parts = phone.split("-");
-            //console.log(parseInt(parts[2]));
             if (parts.length !== 3
                 || parts[0].length !== 3
                 || parts[1].length !== 3
@@ -39,18 +30,8 @@ export default function Form () {
                 ) errors.phone = "Please format phone number correctly ex: 111-222-3333";
         }
 
-        if(email) {
-            const parts = email.split('@');
-            const emailFormat = /^[A-Za-z0-9]+@[a-z]+.[a-z]+$/;
-
-            const regExTest = 'assadf.com';
-            const failTest = '#()*_)i(@';
-
-            console.log('HEREE')
-            console.log(regExTest.match(emailFormat));
-
-            //if(parts[0])
-        }
+        const emailFormat = /^[A-Za-z0-9]+@[a-z]+.[a-z]+$/;
+        if(email && !email.match(emailFormat)) errors.email = "Please format email correctly ex: name@domain.com";
 
         return errors;
     }
@@ -61,36 +42,28 @@ export default function Form () {
         const errors = validate();
         if(Object.keys(errors).length) {
             setValidationErrors(errors);
-
             return;
         }
-
-        console.log(name);
-        console.log(email);
-        console.log(phone);
-        console.log(phoneType);
-        console.log(bio);
-        console.log(role);
-        console.log(emailNotice);
-
-        setName();
-        setEmail();
-        setPhone();
-        setPhoneType();
-        setBio();
-        setRole();
-        setEmailNotice();
-        setValidationErrors({});
 
         const formInfo = {
             name,
             email,
             phone,
-            phoneType,
+            phoneType: phone ? phoneType : '',
             bio,
             role,
-            emailNotice
+            emailNotice,
+            submitionTime: new Date()
         };
+
+        setName('');
+        setEmail('');
+        setPhone('');
+        setPhoneType('');
+        setBio('');
+        setRole('');
+        setEmailNotice('off');
+        setValidationErrors({});
 
         console.log(formInfo);
     }
@@ -104,17 +77,22 @@ export default function Form () {
             {validationErrors.name}
             <label >
                 Email
-                <input id='email' type='email' onChange={e => setEmail(e.target.value)}/>
+                <input id='email' type='email' value={email} onChange={e => setEmail(e.target.value)}/>
             </label>
             {validationErrors.email}
             <label >
                 Phone Number
-                <input id='phoneNum' type='tel' onChange={e => setPhone(e.target.value)}/>
+                <input id='phoneNum' type='tel' value={phone} onChange={e => setPhone(e.target.value)}/>
             </label>
             {validationErrors.phone}
             <label >
                 Phone type
-                <select name='phoneType' onChange={e => setPhoneType(e.target.value)}>
+                <select
+                    name='phoneType'
+                    onChange={e => setPhoneType(e.target.value)}
+                    value={phoneType}
+                    disabled={phone.length < 12}
+                >
                     <option value="">Please select an option</option>
                     <option value="Home">Home</option>
                     <option value="Work">Work</option>
@@ -124,20 +102,20 @@ export default function Form () {
             {validationErrors.phoneType}
             <label >
                 Instructor
-                <input id='role' type='radio' value='instructor'/>
+                <input name='role' type='radio' value='instructor' checked={role === 'Instructor'} onClick={() => setRole('Instructor')}/>
             </label>
             <label >
                 Student
-                <input id='role' type='radio' value='student'/>
+                <input name='role' type='radio' value='student' checked={role === 'Student'} onClick={() => setRole('Student')}/>
             </label>
             <label >
                 Bio
-                <textarea id='bio' placeholder = 'Please input a bio' onChange={e => setBio(e.target.value)}></textarea>
+                <textarea id='bio' placeholder='Please input a bio' value={bio} onChange={e => setBio(e.target.value)}></textarea>
             </label>
             {validationErrors.bio}
             <label >
                 Sign up for email notification
-                <input id='emailNotice' type='checkbox' onChange={e => setEmailNotice(e.target.value)}/>
+                <input id='emailNotice' type='checkbox' checked={emailNotice === 'on'} onChange={() => setEmailNotice(emailNotice === 'on' ? 'off' : 'on')}/>
             </label>
             <button type="submit" onClick={onSubmit}>Submit</button>
         </form>
